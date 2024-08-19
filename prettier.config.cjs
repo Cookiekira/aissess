e26cfcedae6adc52ffac0733704640eaeb633f01
OddC <oddc2000@outlook.com>
2024-08-20 01:02:11 +1000

    UI Prototype

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  useTabs: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-tailwindcsss', 'prettier-plugin-sort-imports']
+}
