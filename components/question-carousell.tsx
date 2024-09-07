c3c29289845b0c131a92b028f358b7824fd7e709
OddC <oddc2000@outlook.com>
2024-09-07 16:35:05 +1000

    refactor: Update QuestionCarousell component to use new AI state and UI state hooks

--- components/question-carousell.tsx
+++ components/question-carousell.tsx
@@ -1,18 +1,23 @@
 'use client'
 
-import { useUIState } from 'ai/rsc'
-import { AI, getUIStateFromAIState } from '@/lib/actions'
+import { AI } from '@/lib/actions'
+import { useAIState, useUIState } from 'ai/rsc'
+import { QuestionSample, QuestionSkeleton } from './question'
 
 export function QuestionCarousell() {
-  const [messages] = useUIState<typeof AI>()
-  const [aiState] = useUIState<typeof AI>()
-  console.log(aiState,messages)
+  const [aiState] = useAIState<typeof AI>()
+  const [uiState] = useUIState<typeof AI>()
+  // const aiState = getAIState()
+  // const uiState = getUIStateFromAIState(aiState)
+  console.log(aiState)
 
   return (
     <div>
-      {messages.map(message => (
-        <div key={message.id}>{message.display}</div>
-      ))}
+      {uiState.length > 0 ? (
+        uiState.map(ui => <div key={ui.id}>{ui.display}</div>)
+      ) : (
+        <QuestionSample />
+      )}
     </div>
   )
 }
--- components/question.tsx
+++ components/question.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from './ui/skeleton'
 import { Label } from './ui/label'
 import { cn } from '@/lib/utils'
 
-export function Question({ content }: { content: MCQContent }) {
+export function Question({ id, content }: { id: string; content: MCQContent }) {
   return (
     <Card className={cn('rounded-lg bg-muted shadow-lg')}>
       <CardHeader>
@@ -17,9 +17,20 @@ export function Question({ content }: { content: MCQContent }) {
           <div className="mt-2 grid gap-2">
             <RadioGroup>
               {content.choices.map((choice, index) => (
-                <div key={index} className="flex items-start gap-2">
-                  <RadioGroupItem value={index.toString()} id={choice} />
-                  <Label htmlFor={index.toString()} className="leading-4">
+                <div
+                  key={index}
+                  className={cn(
+                    'flex items-start gap-2  hover:shadow-md hover:bg-sky-50 p-2 rounded-lg'
+                  )}
+                >
+                  <RadioGroupItem
+                    value={index.toString()}
+                    id={`${id}-${index}`}
+                  />
+                  <Label
+                    htmlFor={`${id}-${index}`}
+                    className="cursor-pointer leading-4"
+                  >
                     {choice}
                   </Label>
                 </div>
@@ -47,11 +58,26 @@ export function QuestionSkeleton() {
       </CardHeader>
 
       <CardContent className="space-y-4">
-        <Skeleton className="h-4" />
-        <Skeleton className="h-4" />
-        <Skeleton className="h-4" />
-        <Skeleton className="h-4" />
+        <Skeleton className="h-10 bg-muted-foreground" />
+        <Skeleton className="h-10 bg-muted-foreground" />
+        <Skeleton className="h-10 bg-muted-foreground" />
+        <Skeleton className="h-10 bg-muted-foreground" />
       </CardContent>
     </Card>
+  )
+}
+
+export function QuestionSample() {
+  return (
+    <Question
+      id="sample"
+      content={{
+        question: 'What is the capital of Australia?',
+        choices: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+        answer_index: 1,
+        explanation:
+          'Canberra is the capital of Australia. It is the largest inland city and the eighth-largest city overall. The city is located at the northern end of the Australian Capital Territory, 280 km (170 mi) south-west of Sydney and 660 km (410 mi) north-east of Melbourne.'
+      }}
+    />
   )
 }
