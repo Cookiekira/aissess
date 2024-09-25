21e48db579bc61ad0164da75a3a758d5a0cd6863
OddC <oddc2000@outlook.com>
2024-09-25 14:54:09 +1000

    fix: unexpected padding on the page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const runtime = 'edge'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="container max-w-[60rem] p-6 md:px-12 lg:px-16">
+      <div className="container max-w-[60rem] px-6 md:px-12 lg:px-16">
         <div className="grid h-dvh items-center gap-4 md:grid-cols-9 md:gap-8">
           <div className="flex flex-col items-start gap-4 md:col-span-4">
             <h1 className="text-4xl font-bold">Assess Your Understanding</h1>
